b841e0b5067cce0943a457d2a2ba3d673ae39794
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-10-19 20:18:44 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,4 +133,5 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.compose.runtime:runtime-livedata:1.7.4")
+
 }
